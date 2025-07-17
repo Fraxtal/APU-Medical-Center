abe9c303f75353761625ab32b1144ba51f3530c9
Fraxtal <nicholaspangts@gmail.com>
2025-07-17 12:13:58 +0800

    Customer file update

--- assignment/src/Customer/Customer.java
+++ assignment/src/Customer/Customer.java
@@ -1,30 +1,38 @@
+package Customer;
+
 import User.User;
 import java.util.ArrayList;
+import java.io.File;
+import java.util.Scanner;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class Customer extends User {
 
-    private ArrayList<Integer> ids;
+    private ArrayList<ArrayList<String>> data;
 
     public Customer(int id, String username, String email) {
         super(id, username, email);
     }
 
-    public static void register(String Username, String Email, String Password, String Address, String ContactNo) {
+    public boolean register(String Username, String Email, String Password, String Address, String ContactNo) {
 
         loadDatabase();
+        LocalDate CurrentDate = LocalDate.now();
+        id = data.size();
+        String date = CurrentDate.toString();
 
         try (FileWriter writer = new FileWriter("assignment\\src\\database\\users.txt", true)) {
-            writer.write("\n");
-            System.out.println("Registration successful!");
+            writer.write(id + "," + Username + "," + Email + "," + Password + "," + Address + "," + ContactNo + "," + date + "\n");
+            return true;
         } catch (IOException e) {
-            System.err.println("Error writing to file: " + e.getMessage());
+            return false;
         }
     }
 
-    private static void loadDatabase() {
-
+    private void loadDatabase() {
+        
     }
 
 }
