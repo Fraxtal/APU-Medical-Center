1fbe39e1dcc11d800f926f3a71e7cbaae2bbc664
02-is-02 <weihaowu89@gmail.com>
2025-09-11 00:55:35 +0800

    Backend Developed

--- assignment/src/Manager/Manager.java
+++ assignment/src/Manager/Manager.java
@@ -4,10 +4,20 @@
  */
 package Manager;
 
+import Manager.Controller.Controller;
+import Manager.Model.Model;
+import java.io.IOException;
+import java.nio.file.Files;
+
 /**
  *
  * @author weiha
  */
 public class Manager {
+    
+    public static void main(String args[])
+    {
+        
+    }
     
 }
